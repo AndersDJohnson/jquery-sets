0b5f0b1a01a9d6330c81bcca074725c7856635fb
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2014-04-07 22:59:42 -0500

    union and intersect tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,13 @@
-test( "hello test", function() {
-  equal($.join('.a').length, 1);
-  equal($.join('.b').length, 2);
-  equal($.join('.a', '.b').length, 3);
+test("union", function() {
+  equal($.union('.a').length, 1);
+  equal($.union('.b').length, 2);
+  equal($.union('.a', '.b').length, 3);
+  equal($('.a').union('.b').length, 3);
+});
+
+test("intersect", function() {
+  equal($.intersect('.a').length, 1);
+  equal($.intersect('.b').length, 2);
+  equal($.intersect('.b', '.z').length, 1);
+  equal($('.b').intersect('.z').length, 1);
 });
